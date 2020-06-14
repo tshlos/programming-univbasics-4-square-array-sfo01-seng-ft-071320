b01617ea8c64c151ccfb355f338e0b43943cd406
Taci Shlosberg <tshlosberg@gmail.com>
2020-06-14 02:12:24 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,10 @@
 def square_array(array)
-  count = 0
-  new_numbers = []
-  
-  while count < array.length 
-  new_numbers.push(array[count]**)
-  
+    count = 0
+    new_numbers = []
+
+    while count < array.length
+        new_numbers.push(array[count] * array[count])
+        count += 1
+    end
+    puts new_numbers 
 end
